01fce366b84e73a5fc51bcf6aacd86e1c0eb3e75
Joshua Gort <j.gort@student.tudelft.nl>
2021-10-08 07:31:16 +0200

    brainfuck: add commands table

--- Assignment 7/brainfuck/brainfuck.s
+++ Assignment 7/brainfuck/brainfuck.s
@@ -2,6 +2,16 @@
 
 format_str: .asciz "We should be executing the following code:\n%s\n\n"
 
+commands:
+	.word	0x015D	#	]	end of loop
+	.word	0x025B	#	[	start of loop
+	.word	0x033E	#	>	move pointer right
+	.word	0x043C	#	<	move pointer left
+	.word	0x052E	#	.	print current pointer
+	.word	0x062D	#	-	dec current pointer
+	.word	0x072C	#	,	get input -> curr pointer
+	.word	0x082B	#	+	inc	current pointer
+
 # Your brainfuck subroutine will receive one argument:
 # a zero termianted string containing the code to execute.
 brainfuck:
@@ -13,7 +23,8 @@ brainfuck:
 	call printf
 	movq $0, %rax
 
-	
+
+
 
 	movq %rbp, %rsp
 	popq %rbp
